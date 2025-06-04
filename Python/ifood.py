1314097455715b547ab7645333619b445310923d
pedroabn <62610839+pedroabn@users.noreply.github.com>
2025-06-03 21:40:29 -0300

    Update ifood.py

--- Python/ifood.py
+++ Python/ifood.py
@@ -1,24 +1,38 @@
-# Classificação por faixa etária
-def classificar_faixa_etaria(Idade):
-    if Idade < 30:
-        return 'Jovem'
-    elif Idade >= 30 <= 59:
-        return 'Adulto'
-    else:
-        return 'Idoso'
+# defs
+#def classificar_faixa_etaria(Idade):
+#    if Idade < 30:
+#        return 'Jovem'
+#    elif Idade >= 30 <= 59:
+#        return 'Adulto'
+#    else:
+#        return 'Idoso'
+
+######################################################
+# Imports e df    
+import numpy as np    
 import pandas as pd
-df = pd.read_csv("ifood_df.csv")
-print(df.columns)
-
+import matplotlib as mtp
+import matplotlib.pyplot as plt
+import seaborn as sns
+df = pd.read_csv("ifood/ifood_df.csv")
+# Análise por Campanhas
+cmp1 = df.drop(columns=["AcceptedCmp1", "AcceptedCmp2", "AcceptedCmp3", "AcceptedCmp4"]).groupby("AcceptedCmp5")
+print(cmp1.describe())
+col = ['Income', 'Kidhome', 'Teenhome', 'Recency','Age','NumCatalogPurchases', 'NumStorePurchases', 'NumWebVisitsMonth']
+cor_cmp1 = cmp1[col].corr()
+sns.heatmap(cor_cmp1, 
+            linewidths=0.2,    # espessura das linhas
+            linecolor='black',  # cor das linhas
+            cmap='coolwarm')  # mapa de cores
+plt.title("Matriz de Correlação")
+plt.show()
 # Agrupamento por idade e soma da renda
-age_income = (
-    df.groupby("Age", as_index=False)["Income"]
-    .sum()
-    .rename(columns={"Income": "Renda","Age":"Idade"})
-    .sort_values("Idade")
-)
-age_income["Idade"] = age_income["Idade"].astype(int)
-print(age_income)
-
-cf = age_income
-cf["Faixa_Etaria"] = cf["Idade"].apply(classificar_faixa_etaria)
+#age_income = (
+#    df.groupby("Age", as_index=False)["Income"]
+#    .sum()
+#    .rename(columns={"Income": "Renda","Age":"Idade"})
+#    .sort_values("Idade")
+#)
+#age_income["Idade"] = age_income["Idade"].astype(int)
+#cf = age_income
+#cf["Faixa_Etaria"] = cf["Idade"].apply(classificar_faixa_etaria)
